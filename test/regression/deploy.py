8f10d9ae56242f1dfe3b79cebf1406c6b744ba93
walterlopatka <75284441+walterlopatka@users.noreply.github.com>
2022-03-04 14:34:59 -0800

    CORTX-28579:  Basic regression tests for cortx-k8s (#126)
    * CORTX-28579: Initial commit of cortx-k8s regression tests
    
    Signed-off-by: Walter Lopatka <wlopatka@gmail.com>

---
+++ test/regression/deploy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+
+from cluster import Cluster
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', dest='solution')
+    parser.add_argument('-c', dest='cluster')
+    args = parser.parse_args()
+
+    cluster = Cluster(args.solution, args.cluster)
+    if args.cluster:
+        result = cluster.run_prereq()
+        if result != 0:
+            sys.exit(1)
+
+    #result = cluster.deploy()
+    #sys.exit(result)
