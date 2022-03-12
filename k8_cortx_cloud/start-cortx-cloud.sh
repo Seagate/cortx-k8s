2d70f5b3966dd3e3cf38649f41367f2c9dc184df
Keith Pine <keith.pine@seagate.com>
2022-03-11 17:35:11 -0800

    Prefer putting braces around variable references even when not strictly required. [SC2250]
    Signed-off-by: Keith Pine <keith.pine@seagate.com>

--- k8_cortx_cloud/start-cortx-cloud.sh
+++ k8_cortx_cloud/start-cortx-cloud.sh
@@ -3,35 +3,35 @@
 solution_yaml=${1:-'solution.yaml'}
 
 # Check if the file exists
-if [ ! -f $solution_yaml ]
+if [ ! -f ${solution_yaml} ]
 then
-    echo "ERROR: $solution_yaml does not exist"
+    echo "ERROR: ${solution_yaml} does not exist"
     exit 1
 fi
 
 function parseSolution()
 {
-    echo "$(./parse_scripts/parse_yaml.sh $solution_yaml $1)"
+    echo "$(./parse_scripts/parse_yaml.sh ${solution_yaml} $1)"
 }
 
 namespace=$(parseSolution 'solution.namespace')
-namespace=$(echo $namespace | cut -f2 -d'>')
+namespace=$(echo ${namespace} | cut -f2 -d'>')
 
 printf "########################################################\n"
 printf "# Start CORTX Control                                   \n"
 printf "########################################################\n"
 num_nodes=0
 while IFS= read -r line; do
-    IFS=" " read -r -a deployments <<< "$line"
-    kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=$namespace
+    IFS=" " read -r -a deployments <<< "${line}"
+    kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=${namespace}
     num_nodes=$((num_nodes+1))
-done <<< "$(kubectl get deployments --namespace=$namespace | grep 'cortx-control')"
+done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-control')"
 
 printf "\nWait for CORTX Control to be ready"
 while true; do
     count=0
     while IFS= read -r line; do
-        IFS=" " read -r -a pod_status <<< "$line"
+        IFS=" " read -r -a pod_status <<< "${line}"
         IFS="/" read -r -a ready_status <<< "${pod_status[1]}"
         if [[ "${pod_status[2]}" != "Running" || "${ready_status[0]}" != "${ready_status[1]}" ]]; then
             if [[ "${pod_status[2]}" == "Error" || "${pod_status[2]}" == "Init:Error" ]]; then
@@ -41,9 +41,9 @@ while true; do
             break
         fi
         count=$((count+1))
-    done <<< "$(kubectl get pods --namespace=$namespace | grep 'cortx-control-')"
+    done <<< "$(kubectl get pods --namespace=${namespace} | grep 'cortx-control-')"
 
-    if [[ $num_nodes -eq $count ]]; then
+    if [[ ${num_nodes} -eq ${count} ]]; then
         break
     else
         printf "."
@@ -59,16 +59,16 @@ printf "# Start CORTX Data                                      \n"
 printf "########################################################\n"
 num_nodes=0
 while IFS= read -r line; do
-    IFS=" " read -r -a deployments <<< "$line"
-    kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=$namespace
+    IFS=" " read -r -a deployments <<< "${line}"
+    kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=${namespace}
     num_nodes=$((num_nodes+1))
-done <<< "$(kubectl get deployments --namespace=$namespace | grep 'cortx-data-')"
+done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-data-')"
 
 printf "\nWait for CORTX Data to be ready"
 while true; do
     count=0
     while IFS= read -r line; do
-        IFS=" " read -r -a pod_status <<< "$line"
+        IFS=" " read -r -a pod_status <<< "${line}"
         IFS="/" read -r -a ready_status <<< "${pod_status[1]}"
         if [[ "${pod_status[2]}" != "Running" || "${ready_status[0]}" != "${ready_status[1]}" ]]; then
             if [[ "${pod_status[2]}" == "Error" || "${pod_status[2]}" == "Init:Error" ]]; then
@@ -78,9 +78,9 @@ while true; do
             break
         fi
         count=$((count+1))
-    done <<< "$(kubectl get pods --namespace=$namespace | grep 'cortx-data-')"
+    done <<< "$(kubectl get pods --namespace=${namespace} | grep 'cortx-data-')"
 
-    if [[ $num_nodes -eq $count ]]; then
+    if [[ ${num_nodes} -eq ${count} ]]; then
         break
     else
         printf "."
@@ -96,16 +96,16 @@ printf "# Start CORTX Server                                    \n"
 printf "########################################################\n"
 num_nodes=0
 while IFS= read -r line; do
-    IFS=" " read -r -a deployments <<< "$line"
-    kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=$namespace
+    IFS=" " read -r -a deployments <<< "${line}"
+    kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=${namespace}
     num_nodes=$((num_nodes+1))
-done <<< "$(kubectl get deployments --namespace=$namespace | grep 'cortx-server-')"
+done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-server-')"
 
 printf "\nWait for CORTX Server to be ready"
 while true; do
     count=0
     while IFS= read -r line; do
-        IFS=" " read -r -a pod_status <<< "$line"
+        IFS=" " read -r -a pod_status <<< "${line}"
         IFS="/" read -r -a ready_status <<< "${pod_status[1]}"
         if [[ "${pod_status[2]}" != "Running" || "${ready_status[0]}" != "${ready_status[1]}" ]]; then
             if [[ "${pod_status[2]}" == "Error" || "${pod_status[2]}" == "Init:Error" ]]; then
@@ -115,9 +115,9 @@ while true; do
             break
         fi
         count=$((count+1))
-    done <<< "$(kubectl get pods --namespace=$namespace | grep 'cortx-server-')"
+    done <<< "$(kubectl get pods --namespace=${namespace} | grep 'cortx-server-')"
 
-    if [[ $num_nodes -eq $count ]]; then
+    if [[ ${num_nodes} -eq ${count} ]]; then
         break
     else
         printf "."
@@ -133,16 +133,16 @@ printf "# Start CORTX HA                                        \n"
 printf "########################################################\n"
 num_nodes=0
 while IFS= read -r line; do
-    IFS=" " read -r -a deployments <<< "$line"
-    kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=$namespace
+    IFS=" " read -r -a deployments <<< "${line}"
+    kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=${namespace}
     num_nodes=$((num_nodes+1))
-done <<< "$(kubectl get deployments --namespace=$namespace | grep 'cortx-ha')"
+done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-ha')"
 
 printf "\nWait for CORTX HA to be ready"
 while true; do
     count=0
     while IFS= read -r line; do
-        IFS=" " read -r -a pod_status <<< "$line"
+        IFS=" " read -r -a pod_status <<< "${line}"
         IFS="/" read -r -a ready_status <<< "${pod_status[1]}"
         if [[ "${pod_status[2]}" != "Running" || "${ready_status[0]}" != "${ready_status[1]}" ]]; then
             if [[ "${pod_status[2]}" == "Error" || "${pod_status[2]}" == "Init:Error" ]]; then
@@ -152,9 +152,9 @@ while true; do
             break
         fi
         count=$((count+1))
-    done <<< "$(kubectl get pods --namespace=$namespace | grep 'cortx-ha')"
+    done <<< "$(kubectl get pods --namespace=${namespace} | grep 'cortx-ha')"
 
-    if [[ $num_nodes -eq $count ]]; then
+    if [[ ${num_nodes} -eq ${count} ]]; then
         break
     else
         printf "."
@@ -167,27 +167,27 @@ printf "\n\n"
 
 function extractBlock()
 {
-    echo "$(./parse_scripts/yaml_extract_block.sh $solution_yaml $1)"
+    echo "$(./parse_scripts/yaml_extract_block.sh ${solution_yaml} $1)"
 }
 
 num_motr_client=$(extractBlock 'solution.common.motr.num_client_inst')
 
-if [[ $num_motr_client -gt 0 ]]; then
+if [[ ${num_motr_client} -gt 0 ]]; then
     printf "########################################################\n"
     printf "# Start CORTX Client                                    \n"
     printf "########################################################\n"
     num_nodes=0
     while IFS= read -r line; do
-        IFS=" " read -r -a deployments <<< "$line"
-        kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=$namespace
+        IFS=" " read -r -a deployments <<< "${line}"
+        kubectl scale deploy "${deployments[0]}" --replicas 1 --namespace=${namespace}
         num_nodes=$((num_nodes+1))
-    done <<< "$(kubectl get deployments --namespace=$namespace | grep 'cortx-client-')"
+    done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-client-')"
 
     printf "\nWait for CORTX Client to be ready"
     while true; do
         count=0
         while IFS= read -r line; do
-            IFS=" " read -r -a pod_status <<< "$line"
+            IFS=" " read -r -a pod_status <<< "${line}"
             IFS="/" read -r -a ready_status <<< "${pod_status[1]}"
             if [[ "${pod_status[2]}" != "Running" || "${ready_status[0]}" != "${ready_status[1]}" ]]; then
                 if [[ "${pod_status[2]}" == "Error" || "${pod_status[2]}" == "Init:Error" ]]; then
@@ -197,9 +197,9 @@ if [[ $num_motr_client -gt 0 ]]; then
                 break
             fi
             count=$((count+1))
-        done <<< "$(kubectl get pods --namespace=$namespace | grep 'cortx-client-')"
+        done <<< "$(kubectl get pods --namespace=${namespace} | grep 'cortx-client-')"
 
-        if [[ $num_nodes -eq $count ]]; then
+        if [[ ${num_nodes} -eq ${count} ]]; then
             break
         else
             printf "."
