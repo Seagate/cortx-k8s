cd5a16b3960621d624c1e5ff36cd926ca2c54783
Keith Pine <keith.pine@seagate.com>
2022-03-11 17:58:43 -0800

    Double quote to prevent globbing and word splitting. [SC2086]
    Signed-off-by: Keith Pine <keith.pine@seagate.com>

--- k8_cortx_cloud/shutdown-cortx-cloud.sh
+++ k8_cortx_cloud/shutdown-cortx-cloud.sh
@@ -13,11 +13,11 @@ fi
 
 function parseSolution()
 {
-    ./parse_scripts/parse_yaml.sh ${solution_yaml} $1
+    ./parse_scripts/parse_yaml.sh "${solution_yaml}" "$1"
 }
 
 namespace=$(parseSolution 'solution.namespace')
-namespace=$(echo ${namespace} | cut -f2 -d'>')
+namespace=$(echo "${namespace}" | cut -f2 -d'>')
 
 printf "########################################################\n"
 printf "# Shutdown CORTX Control                                \n"
@@ -25,12 +25,12 @@ printf "########################################################\n"
 
 while IFS= read -r line; do
     IFS=" " read -r -a deployments <<< "${line}"
-    kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace=${namespace}
-done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-control')"
+    kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace="${namespace}"
+done <<< "$(kubectl get deployments --namespace="${namespace}" | grep 'cortx-control')"
 
 printf "\nWait for CORTX Control to be shutdown"
 while true; do
-    output=$(kubectl get pods --namespace=${namespace} | grep 'cortx-control-')
+    output=$(kubectl get pods --namespace="${namespace}" | grep 'cortx-control-')
     if [[ "${output}" == "" ]]; then
         break
     else
@@ -48,12 +48,12 @@ printf "########################################################\n"
 
 while IFS= read -r line; do
     IFS=" " read -r -a deployments <<< "${line}"
-    kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace=${namespace}
-done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-data-')"
+    kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace="${namespace}"
+done <<< "$(kubectl get deployments --namespace="${namespace}" | grep 'cortx-data-')"
 
 printf "\nWait for CORTX Data to be shutdown"
 while true; do
-    output=$(kubectl get pods --namespace=${namespace} | grep 'cortx-data-')
+    output=$(kubectl get pods --namespace="${namespace}" | grep 'cortx-data-')
     if [[ "${output}" == "" ]]; then
         break
     else
@@ -71,12 +71,12 @@ printf "########################################################\n"
 
 while IFS= read -r line; do
     IFS=" " read -r -a deployments <<< "${line}"
-    kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace=${namespace}
-done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-server-')"
+    kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace="${namespace}"
+done <<< "$(kubectl get deployments --namespace="${namespace}" | grep 'cortx-server-')"
 
 printf "\nWait for CORTX Server to be shutdown"
 while true; do
-    output=$(kubectl get pods --namespace=${namespace} | grep 'cortx-server-')
+    output=$(kubectl get pods --namespace="${namespace}" | grep 'cortx-server-')
     if [[ "${output}" == "" ]]; then
         break
     else
@@ -94,12 +94,12 @@ printf "########################################################\n"
 
 while IFS= read -r line; do
     IFS=" " read -r -a deployments <<< "${line}"
-    kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace=${namespace}
-done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-ha')"
+    kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace="${namespace}"
+done <<< "$(kubectl get deployments --namespace="${namespace}" | grep 'cortx-ha')"
 
 printf "\nWait for CORTX HA to be shutdown"
 while true; do
-    output=$(kubectl get pods --namespace=${namespace} | grep 'cortx-ha-')
+    output=$(kubectl get pods --namespace="${namespace}" | grep 'cortx-ha-')
     if [[ "${output}" == "" ]]; then
         break
     else
@@ -113,7 +113,7 @@ printf "\n\n"
 
 function extractBlock()
 {
-    ./parse_scripts/yaml_extract_block.sh ${solution_yaml} $1
+    ./parse_scripts/yaml_extract_block.sh "${solution_yaml}" "$1"
 }
 
 num_motr_client=$(extractBlock 'solution.common.motr.num_client_inst')
@@ -125,12 +125,12 @@ if [[ ${num_motr_client} -gt 0 ]]; then
 
     while IFS= read -r line; do
         IFS=" " read -r -a deployments <<< "${line}"
-        kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace=${namespace}
-    done <<< "$(kubectl get deployments --namespace=${namespace} | grep 'cortx-client-')"
+        kubectl scale deploy "${deployments[0]}" --replicas 0 --namespace="${namespace}"
+    done <<< "$(kubectl get deployments --namespace="${namespace}" | grep 'cortx-client-')"
 
     printf "\nWait for CORTX Client to be shutdown"
     while true; do
-        output=$(kubectl get pods --namespace=${namespace} | grep 'cortx-client-')
+        output=$(kubectl get pods --namespace="${namespace}" | grep 'cortx-client-')
         if [[ "${output}" == "" ]]; then
             break
         else
