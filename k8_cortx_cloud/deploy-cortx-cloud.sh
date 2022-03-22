5d0c23c5d5539e05376ea667a30ea9237b600e7d
walterlopatka <75284441+walterlopatka@users.noreply.github.com>
2022-03-22 11:19:16 -0700

    CORTX-29179: Remove common.s3.num_inst from solution.yaml validation (#175)
    * CORTX-29179: Remove common.s3.num_inst from solution.yaml validation
    
    Signed-off-by: Walter Lopatka <wlopatka@gmail.com>
    
    * Remove s3.num_inst remnants
    
    Signed-off-by: Keith Pine <keith.pine@seagate.com>
    
    * Validate example solution files via GitHub action
    
    Signed-off-by: Keith Pine <keith.pine@seagate.com>
    
    * Remove more num inst remnants
    
    Signed-off-by: Keith Pine <keith.pine@seagate.com>
    
    * Bump checkout action version
    
    Signed-off-by: Keith Pine <keith.pine@seagate.com>
    
    Co-authored-by: Keith Pine <keith.pine@seagate.com>

--- k8_cortx_cloud/deploy-cortx-cloud.sh
+++ k8_cortx_cloud/deploy-cortx-cloud.sh
@@ -1030,7 +1030,6 @@ function deployCortxServer()
     printf "########################################################\n"
     local cortxserver_image
     local hax_port
-    local s3_num_inst
     local s3_start_port_num
     local s3_service_type
     local s3_service_ports_http
@@ -1039,7 +1038,6 @@ function deployCortxServer()
     s3_service_type=$(getSolutionValue 'solution.common.external_services.s3.type')
     s3_service_ports_http=$(getSolutionValue 'solution.common.external_services.s3.ports.http')
     s3_service_ports_https=$(getSolutionValue 'solution.common.external_services.s3.ports.https')
-    s3_num_inst="$(getSolutionValue 'solution.common.s3.num_inst')"
     s3_start_port_num="$(getSolutionValue 'solution.common.s3.start_port_num')"
     hax_port="$(getSolutionValue 'solution.common.hax.port_num')"
 
@@ -1071,7 +1069,6 @@ function deployCortxServer()
             --set cortxserver.localpathpvc.name="cortx-server-fs-local-pvc-${node_name}" \
             --set cortxserver.localpathpvc.mountpath="${local_storage}" \
             --set cortxserver.localpathpvc.requeststoragesize="1Gi" \
-            --set cortxserver.s3.numinst="${s3_num_inst}" \
             --set cortxserver.s3.startportnum="${s3_start_port_num}" \
             --set cortxserver.hax.port="${hax_port}" \
             --set cortxserver.secretinfo="secret-info.txt" \
