5d85f56ed0a5be367d79a9b3af144e43444c401e
Keith Pine <keith.pine@seagate.com>
2022-04-14 13:25:46 -0700

    Quote the right-hand side of == in [[ ]] to prevent glob matching. [SC2053]
    Signed-off-by: Keith Pine <keith.pine@seagate.com>

--- k8_cortx_cloud/parse_scripts/parse_yaml.sh
+++ k8_cortx_cloud/parse_scripts/parse_yaml.sh
@@ -62,6 +62,12 @@ else
         # Get the var and val from the tuple
         VAR=$(echo $VAR_VAL_ELEMENT | cut -f1 -d'>')
         # Check is the filter matches the var
+        #
+        # Ignore SC2053: $2 is a filter which can take wildcard (*)
+        # characters, so this comparison is intentionally relying on glob
+        # pattern matching.
+        #
+        # shellcheck disable=SC2053
         if [[ $VAR == $YAML_PATH_FILTER ]]
         then
             # If the OUTPUT is empty set it otherwise append
